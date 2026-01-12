bf7cce991257fb9e37c3aade557c0c9b7c6dbdfa
AnderUrienGoierri <and_uri_tel@goierrieskola.org>
2026-01-12 21:39:58 +0100

    Konexio ezarpenak eguneratu

---
+++ php/DB_konexioa.php
@@ -0,0 +1,29 @@
+<?php
+$zerbitzaria = "localhost";     //host
+$datu_basea = "birtek_db";      // nire datu-basearen izena
+$erabiltzailea = "root";        
+// root soilik baimenak ditu lokalean erabiltzeko, GU ez gera lokalean konektatuko,
+//  beraz root-en baimen berdinak dituen beste erabiltzaile bat sortu behar da.
+$pasahitza = "1MG32025"; 
+
+$dsn = "mysql:host=$zerbitzaria;dbname=$datu_basea;charset=utf8";
+
+try {
+    
+    $konexioa = new PDO($dsn, $erabiltzailea, $pasahitza);
+    $konexioa->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // Log mezua konektatu dela adierazteko (nabigatzaileko kontsolan ikusteko)
+    echo "<script>console.log('Datu-Basera ondo konektatu da');</script>";
+} catch (PDOException $e) {
+    // Log mezua errorea egon dela adierazteko (nabigatzaileko kontsolan ikusteko)
+    echo "<script>console.error('Datu-Basera EZ da konektatu');</script>";
+
+    // OHARRA: 'application/json' goiburua kendu dut, bestela nabigatzaileak 
+    // script-ak testu arrunt bezala erakusten ditu eta ez ditu exekutatzen.
+    
+    // die: horeraino bakarrik (inprimatu eta gelditu)
+    die(json_encode(["ERROREA" => "Datu-Base konexio Errorea: " . $e->getMessage()]));
+}
+?>
+
